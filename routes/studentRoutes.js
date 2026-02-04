c2ccdce44031393a0dabed771e3aa299964560b1
Vaishali_Vala <vaishalivala1703@gmail.com>
2026-02-05 00:41:22 +0530

    add functionalities

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,10 +2,14 @@ const express = require('express');
 const { authenticate} = require('../middleware/auth');
 const {authorize } = require('../middleware/checkAuthority');
 const { getProfile } = require('../controllers/studentController');
+const { getOwnAttendance } = require('../controllers/attendanceController');
 
 const router = express.Router();
 
 // Get own profile (student)
 router.get('/profile', authenticate, authorize(['student']), getProfile);
+
+// Get own attendance percentage (student)
+router.get('/attendance', authenticate, authorize(['student']), getOwnAttendance);
 
 module.exports = router;
