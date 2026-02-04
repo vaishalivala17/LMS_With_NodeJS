730326e1013a024ec1676d4c9855ea52d1b8fa84
Vaishali_Vala <vaishalivala1703@gmail.com>
2026-02-05 00:36:57 +0530

    add functionalities

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,7 +11,7 @@ const login = async (req, res) => {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.json({ token, message: 'Admin login successfully', user: { id: user._id, name: user.name, email: user.email, role: user.role } });
+        res.json({ token, message: `${user.role} login successfully`, user: { id: user._id, name: user.name, email: user.email, role: user.role } });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
